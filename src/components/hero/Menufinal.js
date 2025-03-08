d74485e421b9bf2a5c6125b57f864bff80fcbb8e
Daksh-134 <117268999+Daksh-134@users.noreply.github.com>
2025-03-08 15:18:40 +0530

    New logo

--- src/components/hero/Menufinal.js
+++ src/components/hero/Menufinal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Menufinal.css";
-import logo1 from "../logos/dslogo.png";
+import logo1 from "../logos/logo_IEEE.jpg";
 import logo2 from "../logos/ieeelogo.png";
 import { Menu, MenuItem, MenuButton, SubMenu } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/core.css";
@@ -10,7 +10,7 @@ function Menufinal() {
   return (
     <div style={{ marginTop: "50px" }}>
       <div className="flexhor">
-        <img src={logo1} className="Logo1" />
+        <img src={logo1} className="Logo1"  style={{ width: "120px", height: "auto" }}/>
 
         <Link
           activeClass="active"
@@ -25,16 +25,22 @@ function Menufinal() {
           About
         </Link>
         <div className="a_a_team">
-
-        
-        <Menu menuButton={<MenuButton style={{fontWeight:"600"}}>IEEE SSN</MenuButton>} >
-          <MenuItem>
-            <a href="#IEEEDELHISSN_TEAM" className="a_team">SSN 2024-2025</a>
-          </MenuItem>
-          <MenuItem>
-            <a href="#IEEEDELHISSN_TEAM2" className="a_team">SSN 2024-2025</a>
-          </MenuItem>
-        </Menu>
+          <Menu
+            menuButton={
+              <MenuButton style={{ fontWeight: "600" }}>IEEE SSN</MenuButton>
+            }
+          >
+            <MenuItem>
+              <a href="#IEEEDELHISSN_TEAM" className="a_team">
+                SSN 2023-2024
+              </a>
+            </MenuItem>
+            <MenuItem>
+              <a href="#IEEEDELHISSN_TEAM2" className="a_team">
+                SSN 2024-2025
+              </a>
+            </MenuItem>
+          </Menu>
         </div>
         <Link
           activeClass="active"
@@ -80,8 +86,13 @@ function Menufinal() {
         <a href="#Report" className="headtext">
           Articles
         </a>
-        <a href="https://www.dssywc.in/" className="headtext" target={"_blank"}>
-          DSSYWC 
+        <a
+          href="https://dssywlc.bvicam.in/"
+          className="headtext"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          DSSYWC
         </a>
       </div>
       <img src={logo2} className="Logo2" />
